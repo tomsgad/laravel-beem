90837f7e02a7ec04a2d2af931f56678a18ca2ead
Thomson Maguru <thomson@magurugroup.co.tz>
2022-05-08 12:59:23 +0300

    Update config to group by services

--- config/beem.php
+++ config/beem.php
@@ -1,29 +1,40 @@
 <?php
 
 return [
-    /*
-    |--------------------------------------------------------------------------
-    | Beem SMS API Credentials
-    |--------------------------------------------------------------------------
-    |
-    | Here we set api key and secret key that will be used to send the sms. Get your
-    | credentials from https://sms.beem.africa/#!/dashboard/profile/authentication
-    |
-    */
+    'sms' => [
+        /*
+        |--------------------------------------------------------------------------
+        | Beem SMS API Key
+        |--------------------------------------------------------------------------
+        |
+        | Here we set sms api key that will be used to send the sms. Get your
+        | credentials from https://sms.beem.africa/#!/dashboard/profile/authentication
+        |
+        */
+        'api_key' => env('BEEM_SMS_API_KEY', ''),
 
-    'sms_api_key'    => env('BEEM_SMS_API_KEY', ''),
-    'sms_secret_key' => env('BEEM_SMS_SECRET_KEY', ''),
+        /*
+        |--------------------------------------------------------------------------
+        | Beem SMS Secret Key
+        |--------------------------------------------------------------------------
+        |
+        | Here we set sms secret key that will be used to send the sms. Get your
+        | credentials from https://sms.beem.africa/#!/dashboard/profile/authentication
+        |
+        */
+        'secret_key' => env('BEEM_SMS_SECRET_KEY', ''),
 
-    /*
-    |--------------------------------------------------------------------------
-    | Beem SMS Sender Name
-    |--------------------------------------------------------------------------
-    |
-    | Here we set a sender name that will be used to send the sms. Default
-    | sender name is `INFO`. Please only use sender names that have been registered
-    | or the sms will not be sent.
-    |
-    */
+        /*
+        |--------------------------------------------------------------------------
+        | Beem SMS Sender Name
+        |--------------------------------------------------------------------------
+        |
+        | Here we set a sender name that will be used to send the sms. Default
+        | sender name is `INFO`. Please only use sender names that have been registered
+        | or the sms will not be sent.
+        |
+        */
 
-    'sms_sender_name' => env('BEEM_SMS_SENDER_NAME', ''),
+        'sender_name' => env('BEEM_SMS_SENDER_NAME', ''),
+    ]
 ];
--- tests/Feature/Config/ConfigTest.php
+++ tests/Feature/Config/ConfigTest.php
@@ -1,13 +1,13 @@
 <?php
 
 it('expect sms api key from env file', function () {
-    expect(config('beem.sms_api_key'))->toBe('api_key');
+    expect(config('beem.sms.api_key'))->toBe('api_key');
 });
 
 it('expect sms secret key from env file', function () {
-    expect(config('beem.sms_secret_key'))->toBe('secret_key');
+    expect(config('beem.sms.secret_key'))->toBe('secret_key');
 });
 
 it('expect sms sender name from env file', function () {
-    expect(config('beem.sms_sender_name'))->toBe('MAGURU');
+    expect(config('beem.sms.sender_name'))->toBe('MAGURU');
 });
