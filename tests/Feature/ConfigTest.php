b19455fad2af58c786e8bcfa9183deaa262625a0
tomsgad <tomsgad@users.noreply.github.com>
2022-05-08 02:21:09 +0000

    Fix styling

--- tests/Feature/ConfigTest.php
+++ tests/Feature/ConfigTest.php
@@ -1,13 +1,13 @@
 <?php
 
-it('expect sms api key from env file', function (){
+it('expect sms api key from env file', function () {
     expect(config('beem.sms_api_key'))->toBe('api_key');
 });
 
-it('expect sms secret key from env file', function (){
+it('expect sms secret key from env file', function () {
     expect(config('beem.sms_secret_key'))->toBe('secret_key');
 });
 
-it('expect sms sender from env file', function (){
+it('expect sms sender from env file', function () {
     expect(config('beem.sms_sender_name'))->toBe('MAGURU');
 });
