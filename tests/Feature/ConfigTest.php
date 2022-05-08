a96743d96225dbdd4ee49cf5df64527a7f0b3d7c
Thomson Maguru <thomson@magurugroup.co.tz>
2022-05-08 11:29:25 +0300

    Update config test

--- tests/Feature/ConfigTest.php
+++ tests/Feature/ConfigTest.php
@@ -8,6 +8,6 @@ it('expect sms secret key from env file', function (){
     expect(config('beem.sms_secret_key'))->toBe('secret_key');
 });
 
-it('expect sms sender from env file', function (){
+it('expect sms sender name from env file', function (){
     expect(config('beem.sms_sender_name'))->toBe('MAGURU');
 });
